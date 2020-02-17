03283580f5c10f38522b37bea1c108bd2f11760f
Thomas A. Christensen II <25492070+MillironX@users.noreply.github.com>
2020-02-17 11:34:52 -0800

    Initial commit

---
+++ samples.sh
@@ -0,0 +1,56 @@
+#!/bin/sh
+
+{
+  echo "# cowsay-cows"
+  echo ""
+  echo "cowfiles in the original spirit of cowsay, except that all of these are
+  actually bovine"
+  echo ""
+  echo "## Examples"
+  echo ""
+} > README
+
+cowsay_message() {
+  COW="$1"
+  MESSAGE="$2"
+
+  {
+    echo "### $COW"
+    echo ""
+    echo "\`\`\`plaintext"
+    cowsay -f "$COW" "$MESSAGE"
+    echo "\`\`\`"
+    echo ""
+  } >> README
+}
+
+cowthink_message() {
+  COW="$1"
+  MESSAGE="$2"
+
+  {
+    echo "### $COW"
+    echo ""
+    echo "\`\`\`plaintext"
+    cowthink -f "$COW" "$MESSAGE"
+    echo "\`\`\`"
+    echo ""
+  } >> README
+}
+
+cowsay_message bell "The hills are alive with the sound of moo-sic"
+cowsay_message birdstale "Grandpa said there was a moral there, but you had to
+figure it out for yourself"
+cowsay_message bull "Actually, I'm a bull"
+cowsay_message compucow "The programmers number one excuse for slacking off:
+cow-pie-ling"
+cowsay_message cownterfeit "The love of moo-lah is the root of all evil"
+cowsay_message cowpooling "Maybe Al Gore will forgive us for all those methane
+emissions this way"
+cowsay_message cowstruction "My favorite part is bull-dozing"
+cowsay_message longhorn "I've been sent to spread the message: God bless Texas!"
+cowsay_message papal "I forgive you, my calf"
+cowsay_message pie "Nobody makes them like Aunt Aggie"
+cowthink_message resting "Insert deep thoughts and ruminations here"
+cowsay_message super "If Impossible is as good as beef, then Bizarro is as good
+as me"
